c84d66301ab9295cb03f4261a3f8e1547cfe7ab9
renanrcs <renan.cavalcante@yahoo.com>
2026-04-28 07:02:08 -0300

    add Customer and CustomerId classes for customer registration

---
+++ src/main/java/dio/marketplace/registration/domain/Customer.java
@@ -0,0 +1,23 @@
+package dio.marketplace.registration.domain;
+
+import org.springframework.util.Assert;
+
+public class Customer {
+    private CustomerId id;
+    private String name;
+    private String email;
+
+    public Customer(CustomerId id, String name, String email) {
+        Assert.notNull(name, "Customer name cannot be null");
+        Assert.notNull(email, "Customer email cannot be null");
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Customer(String name, String email) {
+        this(new CustomerId(), name, email);
+    }
+
+}
---
+++ src/main/java/dio/marketplace/registration/domain/CustomerId.java
@@ -0,0 +1,16 @@
+package dio.marketplace.registration.domain;
+
+import org.springframework.util.Assert;
+
+import java.util.UUID;
+
+public record CustomerId(UUID id) {
+
+    public CustomerId {
+        Assert.notNull(id, "CustomerId cannot be null");
+    }
+
+    public CustomerId() {
+        this(UUID.randomUUID());
+    }
+}
