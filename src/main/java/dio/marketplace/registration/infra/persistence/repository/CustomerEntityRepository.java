19f5b509ec1386de3bffdaa020dc3fbd2b06b20a
resousadev <resousadev@gmail.com>
2026-04-29 07:17:35 -0300

    Add custom query method to find CustomerEntity by first name prefix and disable delete operation

--- src/main/java/dio/marketplace/registration/infra/persistence/repository/CustomerEntityRepository.java
+++ src/main/java/dio/marketplace/registration/infra/persistence/repository/CustomerEntityRepository.java
@@ -3,10 +3,18 @@ package dio.marketplace.registration.infra.persistence.repository;
 import dio.marketplace.registration.infra.persistence.entity.CustomerEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
 import java.util.UUID;
 
 @RepositoryRestResource
 public interface CustomerEntityRepository extends PagingAndSortingRepository<CustomerEntity, UUID>, CrudRepository<CustomerEntity, UUID> {
+    List<CustomerEntity> findByFirstNameStartsWithIgnoreCase(@Param("firstName") String firstName);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(UUID uuid);
 }
