1609163a2c3c012e64c756b7433189b282896b2a
resousadev <resousadev@gmail.com>
2026-04-28 21:53:40 -0300

    Add validation annotations to Customer entity and include validation starter in build.gradle

--- src/main/java/dio/marketplace/registration/infra/persistence/entity/Customer.java
+++ src/main/java/dio/marketplace/registration/infra/persistence/entity/Customer.java
@@ -1,9 +1,13 @@
 package dio.marketplace.registration.infra.persistence.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -15,8 +19,18 @@ public class Customer {
     @Id
     private UUID id;
 
+    @NotBlank
+    @Column(nullable = false)
     private String firstName;
+
     private String lastName;
+
+    @NotBlank
+    @Email
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private Instant createdAt;
 }
