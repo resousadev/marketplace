834a26966fc5ecba1ae0d6d1046e5fde4678009e
resousadev <resousadev@gmail.com>
2026-04-29 06:44:46 -0300

    Extend CustomerEntityRepository to support paging and sorting

--- src/main/java/dio/marketplace/registration/infra/persistence/repository/CustomerEntityRepository.java
+++ src/main/java/dio/marketplace/registration/infra/persistence/repository/CustomerEntityRepository.java
@@ -2,9 +2,11 @@ package dio.marketplace.registration.infra.persistence.repository;
 
 import dio.marketplace.registration.infra.persistence.entity.CustomerEntity;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
 @RepositoryRestResource
-public interface CustomerEntityRepository extends CrudRepository<CustomerEntity, UUID> {
+public interface CustomerEntityRepository extends PagingAndSortingRepository<CustomerEntity, UUID>, CrudRepository<CustomerEntity, UUID> {
 }
