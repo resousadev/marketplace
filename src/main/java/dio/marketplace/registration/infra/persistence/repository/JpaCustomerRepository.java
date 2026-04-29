521011a3bfb81a7532a145b5296022a0ff4c8b4b
resousadev <resousadev@gmail.com>
2026-04-28 22:01:59 -0300

    Implement JpaCustomerRepository with CustomerEntityRepository integration

--- src/main/java/dio/marketplace/registration/infra/persistence/repository/JpaCustomerRepository.java
+++ src/main/java/dio/marketplace/registration/infra/persistence/repository/JpaCustomerRepository.java
@@ -2,10 +2,19 @@ package dio.marketplace.registration.infra.persistence.repository;
 
 import dio.marketplace.registration.domain.Customer;
 import dio.marketplace.registration.domain.CustomerRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class JpaCustomerRepository implements CustomerRepository {
+
+    private final CustomerEntityRepository customerEntityRepository;
+
+    public JpaCustomerRepository(CustomerEntityRepository customerEntityRepository) {
+        this.customerEntityRepository = customerEntityRepository;
+    }
+
     @Override
     public Customer save(Customer customer) {
         return null;
