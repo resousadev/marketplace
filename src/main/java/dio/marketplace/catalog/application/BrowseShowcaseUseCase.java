b86603ded594b90247e2155ae51e0f1dd7624c5b
resousadev <resousadev@gmail.com>
2026-05-16 18:40:16 -0300

    feat: implement event enrichment with asynchronous processing and enable virtual threads

--- src/main/java/dio/marketplace/catalog/application/BrowseShowcaseUseCase.java
+++ src/main/java/dio/marketplace/catalog/application/BrowseShowcaseUseCase.java
@@ -4,6 +4,7 @@ import dio.marketplace.catalog.domain.Event;
 import dio.marketplace.catalog.domain.EventMetadataRepository;
 import dio.marketplace.catalog.domain.EventRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,24 +13,17 @@ import java.util.List;
 @Service
 public class BrowseShowcaseUseCase {
   private final EventRepository eventRepository;
-  private final EventMetadataRepository eventMetadataRepository;
+  private final EventEnricher eventEnricher;
 
-  public BrowseShowcaseUseCase(EventRepository eventRepository, EventMetadataRepository eventMetadataRepository) {
+  public BrowseShowcaseUseCase(EventRepository eventRepository, EventEnricher eventEnricher) {
     this.eventRepository = eventRepository;
-    this.eventMetadataRepository = eventMetadataRepository;
+    this.eventEnricher = eventEnricher;
   }
 
   public List<Event> execute() {
     var events = eventRepository.findAll()
         .stream()
-        .map(event -> {
-          log.info("Enriching event: {}", event);
-
-          var metadata = eventMetadataRepository.findByEventId(event.getId());
-          event.setMetadata(metadata);
-
-          return event;
-        }).toList();
+        .map(eventEnricher::enrich).toList();
 
     return events;
   }
---
+++ src/main/java/dio/marketplace/catalog/application/EventEnricher.java
@@ -0,0 +1,28 @@
+package dio.marketplace.catalog.application;
+
+import dio.marketplace.catalog.domain.Event;
+import dio.marketplace.catalog.domain.EventMetadataRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class EventEnricher {
+  private final EventMetadataRepository eventMetadataRepository;
+
+  public EventEnricher(EventMetadataRepository eventMetadataRepository) {
+    this.eventMetadataRepository = eventMetadataRepository;
+  }
+
+  @Async
+  public Event enrich(Event event) {
+    log.info("Enriching event: {}", event);
+
+    var metadata = eventMetadataRepository.findByEventId(event.getId());
+    event.setMetadata(metadata);
+
+    return event;
+  }
+
+}
