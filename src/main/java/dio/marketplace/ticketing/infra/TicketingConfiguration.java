7d8da0b2502dd74766d20cb516762a0d588cd2e9
resousadev <resousadev@gmail.com>
2026-05-23 15:48:35 -0300

    feat: Add TicketingConfiguration for database setup and JPA integration

---
+++ src/main/java/dio/marketplace/ticketing/infra/TicketingConfiguration.java
@@ -0,0 +1,69 @@
+package dio.marketplace.ticketing.infra;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceProperties;
+import org.springframework.boot.jpa.EntityManagerFactoryBuilder;
+import org.springframework.boot.jpa.autoconfigure.JpaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.LinkedHashMap;
+
+@Configuration(proxyBeanMethods = false)
+@EnableJpaRepositories(
+    basePackages = "dio.marketplace.ticketing",
+    entityManagerFactoryRef = "ticketingEntityManagerFactory",
+    transactionManagerRef = "ticketingTransactionManager"
+)
+public class TicketingConfiguration {
+
+  @Qualifier("ticketingDataSourceProperties")
+  @Bean(defaultCandidate = false)
+  @ConfigurationProperties(prefix = "spring.ticketing.datasource")
+  public DataSourceProperties ticketingDataSourceProperties() {
+    // Configuração das propriedades do DataSource para o módulo de ticketing
+    return new DataSourceProperties();
+  }
+
+  @Qualifier("ticketingDataSource")
+  @Bean(defaultCandidate = false)
+  @ConfigurationProperties("spring.ticketing.datasource.hikari")
+  public HikariDataSource ticketingDataSource(@Qualifier("ticketingDataSourceProperties") DataSourceProperties properties) {
+    // Configuração do DataSource para o módulo de ticketing
+    return properties.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+  }
+
+  @Qualifier("ticketingEntityManagerFactory")
+  public LocalContainerEntityManagerFactoryBean ticketingEntityManagerFactory(
+      @Qualifier("ticketingDataSource") HikariDataSource dataSource,
+      @Qualifier("ticketingJpaProperties") JpaProperties jpaProperties
+  ) {
+    // Configuração do EntityManagerFactory para o módulo de ticketing
+    var builder = new EntityManagerFactoryBuilder(
+        new HibernateJpaVendorAdapter(),
+        x -> new LinkedHashMap<>(jpaProperties.getProperties()),
+        null
+    );
+
+    return builder
+        .dataSource(dataSource)
+        .packages("dio.marketplace.ticketing")
+        .persistenceUnit("ticketing")
+        .build();
+  }
+
+  public PlatformTransactionManager ticketingTransactionManager(
+      @Qualifier("ticketingEntityManagerFactory") LocalContainerEntityManagerFactoryBean emf
+  ) {
+    // Configuração do TransactionManager para o módulo de ticketing
+    return new JpaTransactionManager(emf.getObject());
+  }
+
+}
