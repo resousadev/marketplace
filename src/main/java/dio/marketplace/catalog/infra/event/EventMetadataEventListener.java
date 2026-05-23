9eb6c38d8046c222fdfacc47cddbb43574b334e3
resousadev <resousadev@gmail.com>
2026-05-23 10:50:46 -0300

    feat: Implement event publishing for customer and event updates

--- src/main/java/dio/marketplace/catalog/infra/event/EventMetadataEventListener.java
+++ src/main/java/dio/marketplace/catalog/infra/event/EventMetadataEventListener.java
@@ -1,8 +1,10 @@
 package dio.marketplace.catalog.infra.event;
 
 import dio.marketplace.catalog.infra.persistence.entity.EventMetadata;
+import dio.marketplace.common.infra.event.dto.EventUpdated;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
 import org.springframework.data.mongodb.core.mapping.event.AfterDeleteEvent;
 import org.springframework.data.mongodb.core.mapping.event.AfterSaveEvent;
@@ -12,9 +14,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class EventMetadataEventListener extends AbstractMongoEventListener<EventMetadata> {
 
+    private final ApplicationEventPublisher eventPublisher;
+
+    public EventMetadataEventListener(ApplicationEventPublisher eventPublisher) {
+        this.eventPublisher = eventPublisher;
+    }
+
     @Override
     public void onAfterSave(@NonNull AfterSaveEvent<EventMetadata> eventMetadata) {
         log.info("EventMetadata saved: {}", eventMetadata.getDocument());
+        eventPublisher.publishEvent(EventUpdated.from(eventMetadata.getSource()));
     }
 
     @Override
---
+++ src/main/java/dio/marketplace/common/infra/event/dto/CustomerCreated.java
@@ -0,0 +1,4 @@
+package dio.marketplace.common.infra.event.dto;
+
+public record CustomerCreated(String id, String name) {
+}
